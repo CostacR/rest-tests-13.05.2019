a08ce0883126f5475e1fec6a06910d69abe19137
CostaCore <konstantin.koresh@gmail.com>
2019-05-23 17:16:45 +0300

    Lesson 4 HW

--- src/test/java/PetStoreTest.java
+++ src/test/java/PetStoreTest.java
@@ -1,3 +1,4 @@
+import com.sun.javafx.scene.traversal.Direction;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
@@ -12,16 +13,13 @@ public class PetStoreTest {
     static {
         RestAssured.baseURI = "https://petstore.swagger.io/v2";
     }
-/*
-cjplfnm wbkrk gj енум
-тест который удаляет животное
-нцвџ
-*/
+
+
 
     @Test
     public void getPetByIdTest(){
 
-        int petId = 2;
+        int petId = 102;
 
         ValidatableResponse response = RestAssured.given()
                 .log().uri()
@@ -33,13 +31,51 @@ cjplfnm wbkrk gj енум
     @Test
     public void getPetByStatus(){
 
+        for (Status status : Status.values()) {
+
+            System.out.println(status);
+            ValidatableResponse response = RestAssured.given()
+                    .log().uri()
+                    .param("status", status)
+                    .get(Config.GET_PET_BY_STATUS)
+                    .then()
+                    .log().all()
+                    .statusCode(200);
+        }
+
+
+    }
+    @Test
+    public void deletePetTest(){
+        int petId = 102;
+
         ValidatableResponse response = RestAssured.given()
                 .log().uri()
-                .param("status", Status.AVAILABLE)
-                .get(Config.GET_PET_BY_STATUS)
+                .delete(Config.DELETE_PET_BY_ID, petId)
                 .then()
                 .log().all()
                 .statusCode(200);
-
     }
+
+    int petId = 102;
+    String petName = "Stinger";
+    String status = String.valueOf(Status.AVAILABLE);
+
+
+//    @Test
+//    public void addPetTest(){
+//        PetStoreTest obj = new PetStoreTest();
+//        System.out.println(obj.petId+obj.petName+obj.status);
+////        Object petName = {"id", 102, "name", "Troll"};
+//
+////        ValidatableResponse response = RestAssured.given()
+////                .log().uri()
+////                .param("id", petId)
+////                .param("name", )
+////
+////                .then()
+////                .log().all()
+////                .statusCode(200);
+//    }
+
 }
